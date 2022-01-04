9653b313e97c3a6712a73f13f428b8434afa782b
MinhNCQng <minh.ncqng123@gmail.com>
2022-01-04 17:22:57 +0700

    Clone day1

---
+++ Clone-web/index.js
@@ -0,0 +1,36 @@
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+const left = document.querySelector(".fa-chevron-left");
+const right = document.querySelector(".fa-chevron-right");
+left.addEventListener('click',()=>{
+    plusSlides(-1);
+})
+right.addEventListener('click',()=>{
+    plusSlides(1);
+})
+
+
